e0c1e5e04e90650c2e1f8c9bdb6ef5c28cadd1c3
Vbdiaz <vbdiaz03@gmail.com>
2023-11-21 16:06:45 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [loginData, setLoginData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the server
+    fetch('https://demo-app-final.azurewebsites.net/api/data') // Updated to use proxy
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => setLoginData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Data</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Password</th> 
+          </tr>
+        </thead>
+        <tbody>
+          {loginData.map((login) => (
+            <tr key={login.id}>
+              <td>{login.username}</td>
+              <td>{login.password}</td>
+              {/* Add more cells as needed */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
